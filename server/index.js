c9efd8e36ea4f10ab5637d1574bb14d0a3b58606
yoojeenkim <yoojeenkim@gmail.com>
2022-06-28 18:53:37 -0400

    added socket io

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
-const https = require('https');
+const http = require('http');
 const express = require('express');
+const { Server } = require("socket.io");
 
 const router = require('./router');
 
 const app = express();
-const server = https.createServer(app);
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(router);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+})
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
